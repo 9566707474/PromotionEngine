5f1044327b36023f9a2440b02a143a58f886bff8
Loganathan Rajendran <loganathan.rajendran.gp@gmail.com>
2021-11-09 22:13:32 +0000

    add Models

--- Abc.Promotion.Api/Models/Cart.cs
+++ Abc.Promotion.Api/Models/Cart.cs
@@ -1,6 +1,31 @@
 ﻿namespace Abc.Promotion.Api.Models
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
+    /// <summary>
+    /// Cart class contains list of products, total amount and total discount applied
+    /// </summary>
     public class Cart
     {
+        /// <summary>
+        /// Contructor to intialize the products
+        /// </summary>
+        public Cart() => Products = new List<Product>();
+
+        /// <summary>
+        /// Collection of products
+        /// </summary>
+        public ICollection<Product> Products { get; set; }
+
+        /// <summary>
+        /// Total discount applied
+        /// </summary>
+        public decimal TotalDiscount => Products.Sum(p => p.Discount);
+
+        /// <summary>
+        /// Total price
+        /// </summary>
+        public decimal TotalAmount => Products.Sum(p => p.TotalPrice);
     }
 }
--- Abc.Promotion.Api/Models/Product.cs
+++ Abc.Promotion.Api/Models/Product.cs
@@ -1,6 +1,33 @@
 ﻿namespace Abc.Promotion.Api.Models
 {
+    /// <summary>
+    /// Product class
+    /// </summary>
     public class Product
     {
+        /// <summary>
+        /// Product code
+        /// </summary>
+        public char ProductCode { get; set; }
+
+        /// <summary>
+        /// Product price
+        /// </summary>
+        public decimal Price { get; set; }
+
+        /// <summary>
+        /// Product quantity 
+        /// </summary>
+        public int Quantity { get; set; }
+
+        /// <summary>
+        /// Product total amount
+        /// </summary>
+        public decimal TotalPrice { get; set; }
+
+        /// <summary>
+        /// Applied discount for the product
+        /// </summary>
+        public decimal Discount => (Quantity * Price) - TotalPrice;
     }
 }
