6b40c66d2299d2d0be12523dd00c8ef09b15c2bb
Loganathan Rajendran <loganathan.rajendran.gp@gmail.com>
2021-11-09 23:40:05 +0000

    third rule added

---
+++ Abc.Promotion.Api.Test/Service/ApplyPromoRequestHandlerTest.cs
@@ -0,0 +1,30 @@
+﻿namespace Abc.Promotion.Api.Test.Service
+{
+    using Abc.Promotion.Api.Models;
+    using Abc.Promotion.Api.Service;
+    using Moq;
+    using Xunit;
+
+    public class ApplyPromoRequestHandlerTest
+    {
+        private readonly ApplyPromoRequestHandler underTest;
+        private Mock<IApplyPromo> applyPromo;
+
+        public ApplyPromoRequestHandlerTest()
+        {
+            applyPromo = new Mock<IApplyPromo>();
+            underTest = new ApplyPromoRequestHandler(applyPromo.Object);
+        }
+
+        [Fact]
+        public async void PromoRequestHandlerShouldSuccess()
+        {
+            var request = new Cart();
+
+            applyPromo.Setup(m => m.Execute(request)).Returns(request);
+            var applyPromoRequest = new ApplyPromoRequest() { Cart = request };
+            var result = await underTest.Handle(applyPromoRequest, default);
+            Assert.NotNull(result);
+        }
+    }
+}
--- Abc.Promotion.Api/Service/Rules/ThirdProductRule.cs
+++ Abc.Promotion.Api/Service/Rules/ThirdProductRule.cs
@@ -1,15 +1,39 @@
 ﻿namespace Abc.Promotion.Api.Service.Rules
 {
     using Abc.Promotion.Api.Models;
+    using System.Linq;
 
     /// <summary>
     /// Apply rule C and D for 30
     /// </summary>
     public class ThirdProductRule : IPromoRule
     {
+        private const char productCodeC = 'C';
+
+        private const char productCodeD = 'D';
+
         public Cart Execute(Cart cart)
         {
-            throw new System.NotImplementedException();
+            var productC = cart.Products.FirstOrDefault(p => p.ProductCode == productCodeC);
+            var productD = cart.Products.FirstOrDefault(p => p.ProductCode == productCodeD);
+
+            if ((productC != null && productD != null) && productC.Quantity > 0 && productD.Quantity > 0)
+            {
+                if (productC.Quantity == productD.Quantity)
+                {
+                    productC.TotalPrice = 15 * productC.Quantity;
+                }
+                else if (productC.Quantity > productD.Quantity)
+                {
+                    productC.TotalPrice = (productC.Quantity - productD.Quantity) * productC.Price + productD.Quantity * 15;
+                }
+                else
+                {
+                    productC.TotalPrice = (productD.Quantity - productC.Quantity) * productC.Price + productD.Quantity * 15;
+                }
+            }
+
+            return cart;
         }
 
         public bool IsMatch()
